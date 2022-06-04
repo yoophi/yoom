44434e353bc2d9c4f8909a4a6fe0c2b85e769c5b
Pyunghyuk Yoo <yoophi@gmail.com>
2022-06-04 22:04:55 +0900

    ✨ NEW: setup express

--- src/server.js
+++ src/server.js
@@ -1,1 +1,10 @@
-console.log("hello, world");
+import express from "express";
+
+const app = express();
+
+app.get("/", (_req, res) => {
+  res.send("hello, world");
+});
+
+const handleListen = () => console.log("Listening on http://localhost:3000");
+app.listen(3000, handleListen);
