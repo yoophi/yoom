31fd2db9350cbf0b6bb84a6332710c295a74799e
Pyunghyuk Yoo <yoophi@gmail.com>
2022-06-04 22:08:59 +0900

    ✨ NEW: apply pug and render /

--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (_req, res) => {
-  res.send("hello, world");
-});
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+
+app.get("/", (_req, res) => res.render("home"));
+app.get("/*", (_req, res) => res.redirect("/"));
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
 app.listen(3000, handleListen);
