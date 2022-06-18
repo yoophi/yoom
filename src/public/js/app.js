c80fb79c4d37e1d3a371d4a267f858d0cff84a2c
Pyunghyuk Yoo <yoophi@gmail.com>
2022-06-18 22:05:56 +0900

    ✨ NEW: handle mute, cameraOff button

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,8 +1,12 @@
 const socket = io();
 
 const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
 
 let myStream;
+let muted = false;
+let cameraOff = false;
 
 async function getMedia() {
   try {
@@ -16,4 +20,26 @@ async function getMedia() {
   }
 }
 
+function handleMuteClick() {
+  if (!muted) {
+    muteBtn.innerText = "Unmute";
+    muted = true;
+  } else {
+    muteBtn.innerText = "Mute";
+    muted = false;
+  }
+}
+
+function handleCameraClick() {
+  if (!cameraOff) {
+    cameraBtn.innerText = "Turn Camera On";
+    cameraOff = true;
+  } else {
+    cameraBtn.innerText = "Turn Camera Off";
+    cameraOff = false;
+  }
+}
+
 getMedia();
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
